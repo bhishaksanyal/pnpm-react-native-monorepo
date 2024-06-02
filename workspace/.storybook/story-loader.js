8c3aa2f55bb7dc1238ffd0db07b50b825a7d7908
bhishaksanyal <sanyalbishak93@gmail.com>
2024-06-02 21:34:31 +0530

    Added storybook config

---
+++ workspace/.storybook/story-loader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../apps/weather/src/app/App.stories');
+}
+
+const stories = ['../apps/weather/src/app/App.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
---
+++ workspace/apps/weather/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  core: { builder: 'webpack5' },
+  stories: [
+    '../src/app/**/*.stories.mdx',
+    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-ondevice-actions',
+    '@storybook/addon-ondevice-backgrounds',
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-notes',
+  ],
+};
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
